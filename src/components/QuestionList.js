084290052f8009a8787986b6ee9a6c03c0430dd0
Loyyo <maciejwiencis262@gmail.com>
2020-11-30 23:20:04 +0100

    Little better

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,59 +1,62 @@
-import React, {useState} from 'react'
-import testy from './testy.json'
+import React, { useState } from 'react';
+import testy from './testy.json';
 
 const QuestionList = () => {
-  const [search, setSearch] = useState("");
-  const updateSearch = (e) => {
-    setSearch(e.target.value.toLowerCase().substr(0,100))
-  }
-  let filteredQuestions = testy.questions.filter((e) => {
-    return e.question.toLowerCase().indexOf(search) !== -1;
-  });
-  return (
-    <>
-    <div className="searchBar">
-      <div className="pytanie">
-      Wpisz pytanie:
-      </div>
-      <input className="search" type="text" value={search} onChange={updateSearch}/>
-    </div>
-    <div className="questionList">
-      {filteredQuestions.map((e)=>{
-      return (
-        <div className="qa">
-          <div className="question">
-            {e.question}
-          </div>
-          <div className="answers">
-            {e.answers.map((answer)=>{
-              return <div className="answer">{answer.a}</div>
-            })}
-          </div>
-          <div className="correct">
-            Poprawna odpowiedź to: <strong>{e.correct.toUpperCase()}</strong>
-          </div>
-        </div>
-      )
-    })}
-    {testy.rynek.question.toLowerCase().indexOf(search) !== -1 && 
-      <div className="questionList">
-        <div className="qa">
-        <div className="question">
-          {testy.rynek.question}
-        </div>
-        <div className="answers">
-          {testy.rynek.answers.map((e)=>{
-            return (
-              <div className="answer">{e.a}<strong>{e.b}</strong></div>
-            )
-          })}
-        </div>
-      </div>
-      </div>
-    }
-    </div>
-    </>
-  )
-}
+	const [search, setSearch] = useState('');
+	const updateSearch = (e) => {
+		setSearch(e.target.value.toLowerCase().substr(0, 100));
+	};
+	let filteredQuestions = testy.questions.filter((e) => {
+		return e.question.toLowerCase().indexOf(search) !== -1;
+	});
+	return (
+		<>
+			<div className='searchBar'>
+				<div className='pytanie'>Wpisz pytanie:</div>
+				<input className='search' type='text' value={search} onChange={updateSearch} />
+			</div>
+			<div className='questionList'>
+				{filteredQuestions.map((e) => {
+					return (
+						<div className='qa'>
+							<div className='question'>{e.question}</div>
+							<div className='answers'>
+								{e.answers.map((answer) => {
+									return <div className='answer'>{answer.a}</div>;
+								})}
+							</div>
+							<div className='correct'>
+								Poprawna odpowiedź to: <strong>{e.correct.toUpperCase()}</strong>
+							</div>
+						</div>
+					);
+				})}
+				{testy.rynek.map((el) => {
+					return (
+						<>
+							{el.question.toLowerCase().indexOf(search) !== -1 && (
+								<div className='questionList'>
+									<div className='qa'>
+										<div className='question'>{el.question}</div>
+										<div className='answers'>
+											{el.answers.map((e) => {
+												return (
+													<div className='answer'>
+														{e.a}
+														<strong>{e.b}</strong>
+													</div>
+												);
+											})}
+										</div>
+									</div>
+								</div>
+							)}
+						</>
+					);
+				})}
+			</div>
+		</>
+	);
+};
 
-export default QuestionList
+export default QuestionList;
